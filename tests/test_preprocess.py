fe065d41f98853d6ffbfda84ed2b835ce07faf5b
Deepika05082 <2024ac05082@wilp.bits-pilani.ac.in>
2026-08-24 16:10:34 +0530

    pytest

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,10 +1,10 @@
 from src.preprocess import CatsDogsDataset
 
 def test_dataset_length():
-    ds = CatsDogsDataset("data/train")
+    ds = CatsDogsDataset("data/PetImages")
     assert len(ds) > 0
 
 def test_label_assignment():
-    ds = CatsDogsDataset("data/train")
+    ds = CatsDogsDataset("data/PetImages")
     img, label = ds[0]
     assert label in [0,1]
